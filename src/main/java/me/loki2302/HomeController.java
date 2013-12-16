1da9e6fa1fca53d5054663be60b7846e46acc8ba
loki2302 <loki2302@loki2302.me>
2013-12-16 08:44:51 +0400

    Now using DeferredResult instead of Callable

--- src/main/java/me/loki2302/HomeController.java
+++ src/main/java/me/loki2302/HomeController.java
@@ -3,7 +3,6 @@ package me.loki2302;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
-import java.util.concurrent.Callable;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,11 +10,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.async.DeferredResult;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
     private List<Message> messages = new ArrayList<Message>();
+    private List<PendingRequest> pendingRequests = new ArrayList<PendingRequest>();
     
     public HomeController() {
         messages.add(new Message(UUID.randomUUID().toString(), "Message one"));
@@ -38,51 +39,63 @@ public class HomeController {
     
     @RequestMapping(value = "/messages/{lastMessageId}", method = RequestMethod.GET)
     @ResponseBody
-    public Callable<List<Message>> getMessages(@PathVariable final String lastMessageId) {
-        return new Callable<List<Message>>() {
-            @Override
-            public List<Message> call() throws Exception {
-                synchronized(messages) {                
-                    Message foundMessage = null;
-                    for(Message message : messages) {
-                        if(!message.id.equals(lastMessageId)) {
-                            continue;
-                        }
-                        
-                        foundMessage = message;
-                        break;
-                    }
-                    
-                    if(foundMessage == null) {
-                        throw new RuntimeException("No such message");
-                    }
-                    
-                    int lastMessageIndex = messages.indexOf(foundMessage);
-                    
-                    while(true) {
-                        if(lastMessageIndex + 1 == messages.size()) {
-                            messages.wait();
-                        }
-                        
-                        List<Message> filteredMessages = messages.subList(lastMessageIndex + 1, messages.size());
-                        if(!filteredMessages.isEmpty()) {
-                            return filteredMessages;
-                        }                        
-                    }
-                }
+    public DeferredResult<List<Message>> getMessages(@PathVariable final String lastMessageId) {
+        DeferredResult<List<Message>> deferredResult = new DeferredResult<List<Message>>();
+        
+        List<Message> filteredMessages = getMessagesSince(lastMessageId);
+        if(!filteredMessages.isEmpty()) {
+            deferredResult.setResult(filteredMessages);
+        } else {
+            synchronized(pendingRequests) {
+                pendingRequests.add(new PendingRequest(lastMessageId, deferredResult));
             }
-        };        
+        }
+        
+        return deferredResult;
     }
     
+    private List<Message> getMessagesSince(String lastMessageId) {
+        synchronized(messages) {                
+            Message foundMessage = null;
+            for(Message message : messages) {
+                if(!message.id.equals(lastMessageId)) {
+                    continue;
+                }
+                
+                foundMessage = message;
+                break;
+            }
+            
+            if(foundMessage == null) {
+                throw new RuntimeException("No such message");
+            }
+            
+            int lastMessageIndex = messages.indexOf(foundMessage);
+            
+            if(lastMessageIndex + 1 == messages.size()) {
+                return new ArrayList<Message>();
+            }
+            
+            return messages.subList(lastMessageIndex + 1, messages.size());
+        }
+    }
+        
     @RequestMapping(value = "/messages", method = RequestMethod.POST)
     @ResponseBody
-    public Object postMessage(@RequestBody Message message) {
+    public void postMessage(@RequestBody Message message) {
         synchronized(messages) {
             message.id = UUID.randomUUID().toString();
             messages.add(message);
-            messages.notifyAll();
+            
+            synchronized(pendingRequests) {
+                while(!pendingRequests.isEmpty()) {
+                    PendingRequest pendingRequest = pendingRequests.get(0);
+                    List<Message> filteredMessages = getMessagesSince(pendingRequest.lastMessageId);
+                    pendingRequest.deferredResult.setResult(filteredMessages);
+                    pendingRequests.remove(pendingRequest);
+                }
+            }
         }
-        return null;
     }
     
     public static class Message {
@@ -95,6 +108,16 @@ public class HomeController {
         public Message(String id, String text) {
             this.id = id;
             this.text = text;
+        }
+    }
+    
+    private static class PendingRequest {
+        public String lastMessageId;
+        public DeferredResult<List<Message>> deferredResult;
+        
+        public PendingRequest(String lastMessageId, DeferredResult<List<Message>> deferredResult) {
+            this.lastMessageId = lastMessageId;
+            this.deferredResult = deferredResult;
         }
     }
 }
--- src/main/java/me/loki2302/MvcConfiguration.java
+++ src/main/java/me/loki2302/MvcConfiguration.java
@@ -2,7 +2,6 @@ package me.loki2302;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -10,7 +9,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @EnableWebMvc
-@EnableAsync
 @ComponentScan("me.loki2302")
 public class MvcConfiguration extends WebMvcConfigurerAdapter {           
     @Override
